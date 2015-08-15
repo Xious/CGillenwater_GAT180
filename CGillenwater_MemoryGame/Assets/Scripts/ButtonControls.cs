f94917ae6703a2954b12ed4ef3f7df48334a8c56
Xious <rxious@yahoo.com>
2015-08-14 23:54:49 -0600

    Got Memory Lab Working And Tested

--- CGillenwater_MemoryGame/Assets/Scripts/ButtonControls.cs
+++ CGillenwater_MemoryGame/Assets/Scripts/ButtonControls.cs
@@ -1,22 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ButtonControls : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () 
-	{
-		
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-
-	}
-
 	public void onClickStart()
-	{
+	{	
 		Application.LoadLevel ("Game_Scene");
 	}
 
