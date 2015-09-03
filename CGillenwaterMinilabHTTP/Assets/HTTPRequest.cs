dbda5023e36b0c71e38e6fcb6fa5246579bea987
Xious Ryuzaki <xious.s2.squee@gmail.com>
2015-09-02 20:17:12 -0600

    Finished HTTP Lab

---
+++ CGillenwaterMinilabHTTP/Assets/HTTPRequest.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HTTPRequest : MonoBehaviour {
+
+	// Use this for initialization
+	// Outlet: http://www.reactgames.com/tyler/gat180.php
+	private WWW www;
+	public Text text;
+	void Start () 
+	{
+		string StartingURL = "http://www.reactgames.com/tyler/gat180.php";
+		www = new WWW (StartingURL);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown(0)) 
+		{
+			StartCoroutine(waitForRequest(www));
+		}
+	}
+
+	IEnumerator waitForRequest(WWW www)
+	{
+		yield return www;
+
+		if (www.error == null) 
+		{
+			text.text = www.text;
+			Debug.Log ("WWW ok!: " + www.text);
+		} 
+		else 
+		{
+			Debug.Log("WWW Error: " + www.error);
+		}
+
+	}
+}
